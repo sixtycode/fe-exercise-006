0bf2eb78f71f91bc9e44be8a99b1039b0d7b968f
sixtycode <sagaraperkasa.work@gmail.com>
2024-09-29 20:06:24 +0700

    refactor: desgin tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import './App.css'
 function App() {
   return (
     <>
-      <p>Chores ToDo List</p>
+      <div className="text-center p-6">
+        <h1 className="text-2xl font-semibold">Chores ToDo List</h1>
+      </div>
       <div className="flex items-center mb-4">
         <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
         </input>
@@ -42,15 +44,19 @@ function App() {
         <button><FaTrashAlt /></button>
       </div>
       <p>-----------------------------------------------------------------------</p>
-      <p>Done : 0</p>
-      <p>Add todo</p>
-      <div>
-        <input type="text" id="small-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      <div className="text-center p-2">
+        <h2 className="text-2xl font-semibold">Done : 0</h2>
+      </div>
+      <div className="text-left grid gap-3 grid-cols-1 grid-rows-1">
+        <p>Add todo</p>
+        <input type="text" id="small-input" className="block w-full p-2 border border-gray-300 rounded-lg bg-transparent focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         </input>
       </div>
-      <button>ADD TASK</button>
+      <div className="text-left mt-4">
+        <button>ADD TASK</button>
+      </div>
     </>
   )
 }
 
-export default App
+export default App;

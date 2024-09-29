a79226362dde6e239d628d379585556eea7ee4b5
sixtycode <sagaraperkasa.work@gmail.com>
2024-09-30 00:20:02 +0700

    refactor: checkbox tweaks material tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -10,38 +10,68 @@ function App() {
 
       <div>
         <div className="flex items-center mb-4">
-          <input id="default-checkbox" type="checkbox" value="" className="mr-2 w-4 h-4 md:accent-green">
-          </input>
+            <input id="default-checkbox" type="checkbox" value="" className="peer checked:bg-green appearance-none border border-green mr-2 w-4 h-4 accent-green focus:ring-green dark:focus:ring-green dark:ring-green focus:ring-2 dark:bg-green dark:border-green">
+            </input>
+            <span class="absolute text-white opacity-0 peer-checked:opacity-100 pointer-events-none">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="white" stroke="white" stroke-width="1">
+              <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+            </svg>
+            </span>
             Create Guest Experience mobile check-in
             <button className="p-3 max-w-sm ml-auto bg-transparent text-pink border-pink border-2 hover:border-white rounded-lg shadow-lg flex items-center space-x-4"><FaTrashAlt /></button>
         </div>
         <div className="flex items-center mb-4">
-            <input id="default-checkbox" type="checkbox" value="" className="mr-2 w-4 h-4 md:accent-green">
+            <input id="default-checkbox" type="checkbox" value="" className="peer checked:bg-green appearance-none border border-green mr-2 w-4 h-4 accent-green focus:ring-green dark:focus:ring-green dark:ring-green focus:ring-2 dark:bg-green dark:border-green">
             </input>
+            <span class="absolute text-white opacity-0 peer-checked:opacity-100 pointer-events-none">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="white" stroke="white" stroke-width="1">
+              <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+            </svg>
+            </span>
             Document current CI/CD process
             <button className="p-3 max-w-sm ml-auto bg-transparent text-pink border-pink border-2 hover:border-white rounded-lg shadow-lg flex items-center space-x-4"><FaTrashAlt /></button>
         </div>
         <div className="flex items-center mb-4">
-            <input id="default-checkbox" type="checkbox" value="" className="mr-2 w-4 h-4 md:accent-green">
+            <input id="default-checkbox" type="checkbox" value="" className="peer checked:bg-green appearance-none border border-green mr-2 w-4 h-4 accent-green focus:ring-green dark:focus:ring-green dark:ring-green focus:ring-2 dark:bg-green dark:border-green">
             </input>
+            <span class="absolute text-white opacity-0 peer-checked:opacity-100 pointer-events-none">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="white" stroke="white" stroke-width="1">
+              <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+            </svg>
+            </span>
             Perform Code Review for final Pillow-Talk release
             <button className="p-3 max-w-sm ml-auto bg-transparent text-pink border-pink border-2 hover:border-white rounded-lg shadow-lg flex items-center space-x-4"><FaTrashAlt /></button>
         </div>
         <div className="flex items-center mb-4">
-            <input id="default-checkbox" type="checkbox" value="" className="mr-2 w-4 h-4 md:accent-green">
+            <input id="default-checkbox" type="checkbox" value="" className="peer checked:bg-green appearance-none border border-green mr-2 w-4 h-4 accent-green focus:ring-green dark:focus:ring-green dark:ring-green focus:ring-2 dark:bg-green dark:border-green">
             </input>
+            <span class="absolute text-white opacity-0 peer-checked:opacity-100 pointer-events-none">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="white" stroke="white" stroke-width="1">
+              <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+            </svg>
+            </span>
             Implement new Color Palette from Design Team
             <button className="p-3 max-w-sm ml-auto bg-transparent text-pink border-pink border-2 hover:border-white rounded-lg shadow-lg flex items-center space-x-4"><FaTrashAlt /></button>
         </div>
         <div className="flex items-center mb-4">
-            <input id="default-checkbox" type="checkbox" value="" className="mr-2 w-4 h-4 md:accent-green">
+            <input id="default-checkbox" type="checkbox" value="" className="peer checked:bg-green appearance-none border border-green mr-2 w-4 h-4 accent-green focus:ring-green dark:focus:ring-green dark:ring-green focus:ring-2 dark:bg-green dark:border-green">
             </input>
+            <span class="absolute text-white opacity-0 peer-checked:opacity-100 pointer-events-none">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="white" stroke="white" stroke-width="1">
+              <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+            </svg>
+            </span>
             Fix image uploading process for guest check-in
             <button className="p-3 max-w-sm ml-auto bg-transparent text-pink border-pink border-2 hover:border-white rounded-lg shadow-lg flex items-center space-x-4"><FaTrashAlt /></button>
         </div>
         <div className="flex items-center mb-4">
-            <input id="default-checkbox" type="checkbox" value="" className="mr-2 w-4 h-4 md:accent-green">
+            <input id="default-checkbox" type="checkbox" value="" className="peer checked:bg-green appearance-none border border-green mr-2 w-4 h-4 accent-green focus:ring-green dark:focus:ring-green dark:ring-green focus:ring-2 dark:bg-green dark:border-green">
             </input>
+            <span class="absolute text-white opacity-0 peer-checked:opacity-100 pointer-events-none">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="white" stroke="white" stroke-width="1">
+              <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+            </svg>
+            </span>
             Provide on-boarding documentation
             <button className="p-3 max-w-sm ml-auto bg-transparent text-pink border-pink border-2 hover:border-white rounded-lg shadow-lg flex items-center space-x-4"><FaTrashAlt /></button>
         </div>
